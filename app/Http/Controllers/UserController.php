1730fb2db145be13df815d79c3b7ea53c2080717
Henry Lee <henryleeworld@gmail.com>
2020-05-17 00:44:06 +0800

    feat:初始化

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(1);
+        echo $user->name . ' ' . '拜訪次數：' . visits($user)->count() . PHP_EOL;
+        echo $user->name . ' ' . '新增拜訪 1 次' . PHP_EOL;
+        visits($user)->seconds(1)->increment();
+        sleep(1);
+        echo $user->name . ' ' . '拜訪次數：' . visits($user)->count() . PHP_EOL;
+        echo $user->name . ' ' . '減少拜訪 1 次' . PHP_EOL;
+        visits($user)->seconds(1)->decrement();
+        sleep(1);
+        echo $user->name . ' ' . '拜訪次數：' . visits($user)->count() . PHP_EOL;
+        echo $user->name . ' ' . '新增拜訪 3 次' . PHP_EOL;
+        visits($user)->seconds(1)->increment(3);
+        sleep(1);
+        echo $user->name . ' ' . '拜訪次數：' . visits($user)->count() . PHP_EOL;
+        echo $user->name . ' ' . '減少拜訪 2 次' . PHP_EOL;
+        visits($user)->seconds(1)->decrement(2);
+        sleep(1);
+        echo $user->name . ' ' . '拜訪次數：' . visits($user)->count() . PHP_EOL;
+    }
+}
